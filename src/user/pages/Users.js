2f69e7d5db77215ad92614177af542c110375a48
Joanna Krol <joannakrol@Rustams-MacBook-Air.local>
2020-12-13 22:15:45 +0100

    Add: map modal, google map, places per user

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -5,7 +5,7 @@ import UsersList from "../components/UsersList";
 const Users = () => {
   const USERS = [
     {
-      id: "w2",
+      id: "u1",
       name: "Victoria Shumaher",
       image:
         "https://images.unsplash.com/photo-1549212926-67debc84aa8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
